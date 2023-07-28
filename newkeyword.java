da91c9e37457c6ad6644ceeb57a72f3e911a9c6a
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 10:49:58 +0530

    Create newkeyword.java

---
+++ newkeyword.java
@@ -0,0 +1,15 @@
+/* String in Java */
+class A 
+{
+    public static void main(String[] args) {
+        
+        String a=new String("abhi");
+        System.out.println(a);
+
+        String b=new String("abhi");
+        System.out.println(b);
+
+        a=a.concat("gupta");
+        System.out.println(a);
+    }
+} 
