fd0d3134eae97bbf0a35f1a09a8adff4283ec3a2
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 10:55:32 +0530

    Create Stringcomparison.java

---
+++ Stringcomparison.java
@@ -0,0 +1,18 @@
+/* String Comparison in Java */
+class A 
+{
+    public static void main(String[] args) {
+        
+        String a="Lion"; // String literal
+        String b="Lion";
+
+        if(a==b)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.print("False");
+        }
+    }
+}
