24037f6e3fa469c705fa455089afa0f18cf35159
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-28 10:45:51 +0530

    Create Stringliteral.java

---
+++ Stringliteral.java
@@ -0,0 +1,15 @@
+/* String in Java */
+class A 
+{
+    public static void main(String[] args) {
+        
+        String a="abhi"; // literal
+        System.out.println(a);
+
+        String b="abhi"; // literal
+        System.out.println(b);
+
+        a=a.concat("gupta");
+        System.out.println(a);
+    }
+}
