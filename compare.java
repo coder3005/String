a66670a3b82b7bbfb80ba2ed0806197f61b76658
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:20:35 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,30 @@
+package String;
+
+public class compare {
+    public static void main(String[] args) {
+        String s1="Tony";
+        String s2="Tony";
+        String s3=new String("Tony"); //new tony is created
+
+        if(s1==s2) {
+            System.out.println("Strings are  equal");
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+        if(s1==s3) {
+            System.out.println("Strings are  equal");
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+
+        //equals
+        if(s1.equals(s3)) {
+            System.out.println("Strings are equal");
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
